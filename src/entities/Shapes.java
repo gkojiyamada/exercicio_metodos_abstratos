cef09362fa275411b5ecce85339db4210b2ff850
Koji <koji-yamada@hotmail.com>
2020-09-23 15:19:13 -0300

    Projeto criado

---
+++ src/entities/Shapes.java
@@ -0,0 +1,26 @@
+package entities;
+
+import enums.Color;
+
+public abstract class Shapes {
+
+	private Color color;
+	
+	public Shapes() {
+	}
+
+	public Shapes(Color color) {
+		this.color = color;
+	}
+
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	
+	public abstract double area();
+}
